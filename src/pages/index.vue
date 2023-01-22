c6159a1c25f93cbc38aa6e0ad3ebaf8332620565
Ismael Garcia <ig@reflect-media.de>
2023-01-22 14:12:10 +0100

    update the hero page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -29,7 +29,7 @@ const homePageLinks = [
 </script>
 
 <template>
-  <main class="h-screen main">
+  <main class="main h-screen">
     <nav class="flex w-full h-8 pt-4 font-semibold text-sm uppercase text-gray-800">
       <div class="lg:w-32 pl-4 pt-1">
         <img src="/img/CHATINOLOGO.png" width="300" class="mx-auto rounded-full" />
@@ -59,17 +59,40 @@ const homePageLinks = [
         </a>
       </div>
     </nav>
-    <header class="mt-11 md:mt-30 text-center max-w-5xl mx-auto">
-      <h1 class="text-3xl font-black my-10 leading-loose">{{ t("home.hero.title") }}</h1>
-      <h1 class="text-xl leading-snug">{{ t("home.hero.description") }}</h1>
-    </header>
+    <section class="dark:bg-gray-900 mt-10">
+      <div
+        class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
+      >
+        <div class="mr-auto place-self-center lg:col-span-7">
+          <h1
+            class="mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-2xl xl:text-4xl dark:text-white"
+          >
+            {{ t("home.hero.title") }}
+          </h1>
+          <p class="mb-6 font-light text-gray-500 lg:mb-8 md:text-lg dark:text-gray-400">
+            {{ t("home.hero.description") }}
+          </p>
+          <p class="mb-6 font-light text-gray-500 lg:mb-8 md:text-lg dark:text-gray-400">
+            {{ t("home.hero.smallDescription") }}
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Resources
+          </a>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <img src="img/hero_bg.png" alt="mockup" class="contrast-125 rounded-lg" />
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 <style lang="scss">
 .main {
-  //   background-image: url(https://tesla-cdn.thron.com/delivery/public/image/tesla/da705069-91b5-41cb-86f3-86a585c6fdf3/bvlatuR/std/2880x1800/MX-Hero-Desktop);
   background-size: cover;
-  background: linear-gradient(45deg, rgba(27, 45, 85, 0.933), rgb(26, 17, 110));
+  //   background: linear-gradient(45deg, rgba(27, 45, 85, 0.933), rgb(26, 17, 110));
 }
 </style>
 <route lang="yaml">
