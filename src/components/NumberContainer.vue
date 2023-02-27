0878ea11015c8290c57e5b384765f5e588ed411a
leamsigc <leamsigc@hotmail.com>
2023-02-27 01:24:55 +0100

    Show the numbers

--- src/components/NumberContainer.vue
+++ src/components/NumberContainer.vue
@@ -11,17 +11,24 @@
  * @todo [✔] Update the typescript.
  */
 import data from "@/assets/numbers.json";
+const formattedData = data.sort((a, b) => a.numero - b.numero);
 </script>
 
 <template>
   <div
-    class="h-screen grid place-content-center bg-transparent"
-    v-for="(item, index) in data"
+    class="h-screen grid place-content-center bg-transparent mt-10"
+    v-for="(item, index) in formattedData"
     :key="index"
   >
-    <div class="artboard phone-1 rounded-lg shadow-lg indicator custom-bg">
+    <div
+      class="artboard phone-1 rounded-lg shadow-lg text-center indicator custom-bg text-white flex flex-col justify-center items-center"
+    >
       <span class="indicator-item badge badge-info"></span>
-      {{ item }}
+      <div class="text-9xl text-blue-500 font-black font-mono">{{ item.numero }}</div>
+      <div class="text-5xl font-semibold">{{ item.Chatino }}</div>
+      <div class="text-sm font-thin text-gray-400 mt-4">{{ item.Espanol }}</div>
+      <div class="text-sm font-light text-gray-400 mb-4">{{ item.English }}</div>
+      <div class="text-xl text-gray-300">{{ item.example }}</div>
     </div>
   </div>
 </template>
@@ -29,7 +36,7 @@ import data from "@/assets/numbers.json";
 .custom-bg {
   background-image: radial-gradient(
     rgba(31, 41, 55, 0.2) 0.5px,
-    rgb(242, 242, 242) 0.5px
+    rgba(242, 242, 242, 0.31) 0.5px
   );
   background-position-x: 50%;
   background-position-y: 0%;
