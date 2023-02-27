ce41301831dc70e8495993dee609d972050820ff
leamsigc <leamsigc@hotmail.com>
2023-02-27 01:39:20 +0100

    Update the numbers

--- src/components/NumberContainer.vue
+++ src/components/NumberContainer.vue
@@ -20,15 +20,28 @@ const formattedData = data.sort((a, b) => a.numero - b.numero);
     v-for="(item, index) in formattedData"
     :key="index"
   >
-    <div
-      class="artboard phone-1 rounded-lg shadow-lg text-center indicator custom-bg text-white flex flex-col justify-center items-center"
-    >
-      <span class="indicator-item badge badge-info"></span>
-      <div class="text-9xl text-blue-500 font-black font-mono">{{ item.numero }}</div>
-      <div class="text-5xl font-semibold">{{ item.Chatino }}</div>
-      <div class="text-sm font-thin text-gray-400 mt-4">{{ item.Espanol }}</div>
-      <div class="text-sm font-light text-gray-400 mb-4">{{ item.English }}</div>
-      <div class="text-xl text-gray-300">{{ item.example }}</div>
+    <div class="mockup-phone">
+      <div class="camera"></div>
+      <div class="display">
+        <div
+          class="artboard artboard-demo phone-1 rounded-lg shadow-lg text-center indicator custom-bg text-white flex flex-col justify-center items-center"
+        >
+          <span class="indicator-item badge badge-info"></span>
+          <div class="avatar mb-5">
+            <div class="mask mask-squircle w-10">
+              <img src="/img/logo-sm.png" alt="chatino podcasts" class="mask" />
+            </div>
+          </div>
+          <div class="text-9xl text-blue-500 font-black font-mono">{{ item.numero }}</div>
+          <div class="text-5xl font-semibold">{{ item.Chatino }}</div>
+          <div class="text-sm font-thin text-gray-400 mt-4">{{ item.Espanol }}</div>
+          <div class="text-sm font-light text-gray-400 mb-4">{{ item.English }}</div>
+          <div class="text-xl text-gray-300">{{ item.example }}</div>
+          <div class="text-blue-400 mt-10 text-xs">
+            <a href="leamsigc.com" target="_blank"> Ismael G. </a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
